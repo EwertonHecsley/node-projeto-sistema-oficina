c19aed33c96b3e6ebd849d8a8eeca5c1314c321f
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-04-29 21:17:14 -0300

    feat: Classes de error

--- src/core/domain/objectValue/Email.ts
+++ src/core/domain/objectValue/Email.ts
@@ -1,15 +1,17 @@
+import { BadRequest } from "../../../shared/errors/custom/BadRequest";
+
 export class Email {
-  private constructor(private readonly value: string) {}
+    private constructor(private readonly value: string) { }
 
-  static create(value: string): Email {
-    if (!this.validate(value)) {
-      throw new Error('Invalid email format.');
+    static create(value: string): Email {
+        if (!this.validate(value)) {
+            throw new BadRequest('Invalid email format.');
+        }
+        return new Email(value);
     }
-    return new Email(value);
-  }
 
-  private static validate(email: string): boolean {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(email);
-  }
+    private static validate(email: string): boolean {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email);
+    }
 }
---
+++ src/shared/errors/custom/BadRequest.ts
@@ -0,0 +1,7 @@
+import { GenericErrors } from "../GenericError";
+
+export class BadRequest extends GenericErrors {
+    constructor(message: string = 'Bad Request') {
+        super(message, 400);
+    }
+}

139cd98342ac6bc06f2c38fb2477119377d53509
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-01 17:07:04 -0300

    test: teste para caso de uso listar todos os clientes cadastrados

--- src/tests/consumerClient/findConsumer.spec.ts
+++ src/tests/consumerClient/findConsumer.spec.ts
@@ -1,47 +1,46 @@
-import { ConsumerClient } from "../../core/domain/consumerClient/entity/ConsumerCLient";
-import { DocumentClient } from "../../core/domain/consumerClient/objectValue/Document";
-import { Email } from "../../core/domain/consumerClient/objectValue/Email";
-import { FindConsumerClientUseCase } from "../../core/domain/consumerClient/useCase/FindClient";
-import Identity from "../../core/generics/Identity";
-import { NotFound } from "../../shared/errors/custom/NotFound";
-
+import { ConsumerClient } from '../../core/domain/consumerClient/entity/ConsumerCLient';
+import { DocumentClient } from '../../core/domain/consumerClient/objectValue/Document';
+import { Email } from '../../core/domain/consumerClient/objectValue/Email';
+import { FindConsumerClientUseCase } from '../../core/domain/consumerClient/useCase/FindClient';
+import Identity from '../../core/generics/Identity';
+import { NotFound } from '../../shared/errors/custom/NotFound';
 
 const mockRepository = {
-    getClientById: jest.fn(),
+  getClientById: jest.fn(),
 } as any;
 
 const useCase = new FindConsumerClientUseCase(mockRepository);
 
 describe('FindConsumerClientUseCase', () => {
-    const clientId = 'any-id';
-    const clientMock = ConsumerClient.create(
-        {
-            name: 'Ewerton',
-            city: 'Patos',
-            phone: '83988887777',
-            juridicalPerson: false,
-            docType: DocumentClient.create('09253538414', false),
-            email: Email.create('ewerton@example.com'),
-        },
-        new Identity(clientId)
-    );
+  const clientId = 'any-id';
+  const clientMock = ConsumerClient.create(
+    {
+      name: 'Ewerton',
+      city: 'Patos',
+      phone: '83988887777',
+      juridicalPerson: false,
+      docType: DocumentClient.create('09253538414', false),
+      email: Email.create('ewerton@example.com'),
+    },
+    new Identity(clientId),
+  );
 
-    it('Deve retornar um cliente com o Id passado', async () => {
-        mockRepository.getClientById.mockResolvedValue(clientMock);
+  it('Deve retornar um cliente com o Id passado', async () => {
+    mockRepository.getClientById.mockResolvedValue(clientMock);
 
-        const result = await useCase.execute({ id: clientId });
+    const result = await useCase.execute({ id: clientId });
 
-        expect(result.isRight()).toBe(true);
-        expect(result.value).toEqual(clientMock);
-    });
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual(clientMock);
+  });
 
-    it('Deve retornar um erro NOT FOUND caso o cliente nao exista', async () => {
-        mockRepository.getClientById.mockResolvedValue(null);
+  it('Deve retornar um erro NOT FOUND caso o cliente nao exista', async () => {
+    mockRepository.getClientById.mockResolvedValue(null);
 
-        const result = await useCase.execute({ id: clientId });
+    const result = await useCase.execute({ id: clientId });
 
-        expect(result.isLeft()).toBe(true);
-        expect(result.value).toBeInstanceOf(NotFound);
-        expect((result.value as NotFound).message).toBe('Cliente nao encontrado.');
-    });
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotFound);
+    expect((result.value as NotFound).message).toBe('Cliente nao encontrado.');
+  });
 });

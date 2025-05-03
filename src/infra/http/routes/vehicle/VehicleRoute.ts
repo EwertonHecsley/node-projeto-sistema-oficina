32b00ead912a1f4d9345484fcb5a847619546d1a
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-03 11:10:13 -0300

    feat: rotas da entidade veiculo

--- src/infra/http/routes/vehicle/VehicleRoute.ts
+++ src/infra/http/routes/vehicle/VehicleRoute.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { VehicleController } from '../../controllers/vehicle/VehicleController';
 import { validateBody } from '../../middleware/validate';
 import { createVehicleSchema } from '../../controllers/vehicle/dto/vehicleSchemaDto';
+import { updateVehicleSchema } from '../../controllers/vehicle/dto/vehicleUpdateSchemaDto';
 
 export class VehicleRouter {
   private readonly controller = new VehicleController();
@@ -10,9 +11,23 @@ export class VehicleRouter {
     app.post('/vehicle', {
       preHandler: validateBody(createVehicleSchema),
       handler: this.controller.store.bind(this.controller),
-    }),
-      app.get('/vehicle', {
-        handler: this.controller.list.bind(this.controller),
-      });
+    });
+
+    app.get('/vehicle', {
+      handler: this.controller.list.bind(this.controller),
+    });
+
+    app.get('/vehicle/:id', {
+      handler: this.controller.index.bind(this.controller),
+    });
+
+    app.delete('/vehicle/:id', {
+      handler: this.controller.destroy.bind(this.controller),
+    });
+
+    app.put('/vehicle/:id', {
+      preHandler: validateBody(updateVehicleSchema),
+      handler: this.controller.update.bind(this.controller),
+    });
   }
 }

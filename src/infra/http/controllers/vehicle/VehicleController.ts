e77dfb80e4786af050f1488bb34f2a4fea9d576a
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-03 10:53:27 -0300

    feat: Adicionando demais controller para veiculo

--- src/infra/http/controllers/vehicle/VehicleController.ts
+++ src/infra/http/controllers/vehicle/VehicleController.ts
@@ -4,32 +4,46 @@ import { VehicleOrmRepository } from '../../../database/repository/vehicle/vehic
 import { CreateVehicleDto } from './dto/vehicleSchemaDto';
 import { VehiclePresenter } from '../presenter/vehicle/VehiclePresenter';
 import { ListAllVehiclesUseCase } from '../../../../core/domain/vehicle/useCase/LisAllVehicles';
+import { FindVehicleUseCase } from '../../../../core/domain/vehicle/useCase/findVehicle';
+import { DeleteVehicleUseCase } from '../../../../core/domain/vehicle/useCase/DelteVehicle';
+import { NotFound } from '../../../../shared/errors/custom/NotFound';
+import { UpdateVehicleDto } from './dto/vehicleUpdateSchemaDto';
+import { UpdateVehicleUseCase } from '../../../../core/domain/vehicle/useCase/updateVehicle';
+import { logger } from '../../../../shared/utils/logger';
 
 export class VehicleController {
   private readonly repository = new VehicleOrmRepository();
   private readonly createVehicle = new CreateVehicleUseCase(this.repository);
   private readonly listAllVehicleUseCase = new ListAllVehiclesUseCase(this.repository);
+  private readonly findVehicleUseCase = new FindVehicleUseCase(this.repository);
+  private readonly deleteVehicleUseCase = new DeleteVehicleUseCase(this.repository);
+  private readonly updateVehicleUseCase = new UpdateVehicleUseCase(this.repository);
 
   async store(request: FastifyRequest, reply: FastifyReply): Promise<void> {
     const { plate } = request.body as CreateVehicleDto;
 
+    logger.info('Criando novo veiculo...');
     const result = await this.createVehicle.execute({ plate });
 
     if (result.isLeft()) {
+      logger.error('Erro ao criar veiculo.');
       reply.status(result.value.statusCode).send({ message: result.value.message });
       return;
     }
 
+    logger.info('Veiculo criado com sucesso.');
     reply.status(201).send({
       message: 'Veiculo criado com sucesso.',
       vehicle: VehiclePresenter.toHTTP(result.value),
     });
   }
 
-  async list(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+  async list(_request: FastifyRequest, reply: FastifyReply): Promise<void> {
+    logger.info('Listando veiculos...');
     const result = await this.listAllVehicleUseCase.execute();
 
     if (result.isLeft()) {
+      logger.error('Erro ao criar veiculo.');
       reply.status(500).send({ message: 'Erro interno do servidor.' });
       return;
     }
@@ -38,5 +52,56 @@ export class VehicleController {
       message: 'Lista de veciulos.',
       vehicle: result.value.map((element) => VehiclePresenter.toHTTP(element)),
     });
+  }
+
+  async index(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+    const id = request.params as string;
+
+    logger.info('Buscando veiculo');
+    const result = await this.findVehicleUseCase.execute({ id });
+
+    if (result.isLeft()) {
+      logger.error('Erro ao buscar veiculo.');
+      reply.status(result.value.statusCode).send({ message: result.value.message });
+      return;
+    }
+
+    reply.status(200).send({
+      message: 'Veiculo encontrado com sucesso.',
+      vehicle: VehiclePresenter.toHTTP(result.value),
+    });
+  }
+
+  async destroy(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+    const id = request.params as string;
+
+    logger.info('Deletando veiculo...');
+    const result = await this.deleteVehicleUseCase.execute({ id });
+
+    if (result.isLeft()) {
+      logger.error('Erro ao deletar veiculo.');
+      reply.status(result.value.statusCode).send({ message: result.value.message });
+      return;
+    }
+
+    logger.info('Veiculo deletado com sucesso.');
+    reply.status(204).send();
+  }
+
+  async update(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+    const id = request.params as string;
+    const { plate } = request.body as UpdateVehicleDto;
+
+    logger.info('Atualizando veiculo...');
+    const result = await this.updateVehicleUseCase.execute({ id, plate });
+
+    if (result.isLeft()) {
+      logger.error('Erro ao atualizar veiculo.');
+      reply.status(result.value.statusCode).send({ message: result.value.message });
+      return;
+    }
+
+    logger.info('Veiculo atualizado com sucesso.');
+    reply.status(204).send();
   }
 }

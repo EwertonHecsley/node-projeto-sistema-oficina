bf54da0123fd9f7be67d80bfbe7be5007bf42764
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-08 00:22:36 -0300

    test: teste unitario para listar todos os mecanicos cadastrados

--- src/core/domain/mechanic/useCase/FindAllMechanic.ts
+++ src/core/domain/mechanic/useCase/FindAllMechanic.ts
@@ -1,18 +1,18 @@
-import { GenericErrors } from "../../../../shared/errors/GenericError";
-import { Either, left, right } from "../../../../shared/utils/either";
-import { Mechanic } from "../entity/Mechanic";
-import { MechanicRepository } from "../repository/MechanicRepository";
+import { GenericErrors } from '../../../../shared/errors/GenericError';
+import { Either, left, right } from '../../../../shared/utils/either';
+import { Mechanic } from '../entity/Mechanic';
+import { MechanicRepository } from '../repository/MechanicRepository';
 
 type Response = Either<null | GenericErrors, Mechanic[]>;
 
 export class FindAllMechanicsUseCase {
-    constructor(private readonly mechanicRepository: MechanicRepository) { }
+  constructor(private readonly mechanicRepository: MechanicRepository) {}
 
-    async execute(): Promise<Response> {
-        const mechanics = await this.mechanicRepository.findAll();
+  async execute(): Promise<Response> {
+    const mechanics = await this.mechanicRepository.findAll();
 
-        if (!mechanics) return left(new GenericErrors('Erro interno no servidor', 500));
+    if (!mechanics) return left(new GenericErrors('Erro interno no servidor', 500));
 
-        return right(mechanics);
-    };
+    return right(mechanics);
+  }
 }
--- src/tests/unitTests/mechanic/createMechanic.spec.ts
+++ src/tests/unitTests/mechanic/createMechanic.spec.ts
@@ -6,101 +6,101 @@ import { CreateMechanicUseCase } from '../../../core/domain/mechanic/useCase/Cre
 import { BadRequest } from '../../../shared/errors/custom/BadRequest';
 
 const mockMechanicRepostory = {
-    create: jest.fn(),
-    findByEmail: jest.fn(),
-    findByCpf: jest.fn(),
+  create: jest.fn(),
+  findByEmail: jest.fn(),
+  findByCpf: jest.fn(),
 } as unknown as MechanicRepository;
 
 describe('CreateMechanicUseCase', () => {
-    let createMechanicUSeCase: CreateMechanicUseCase;
+  let createMechanicUSeCase: CreateMechanicUseCase;
 
-    beforeEach(() => {
-        jest.clearAllMocks(),
-            (createMechanicUSeCase = new CreateMechanicUseCase(mockMechanicRepostory));
+  beforeEach(() => {
+    jest.clearAllMocks(),
+      (createMechanicUSeCase = new CreateMechanicUseCase(mockMechanicRepostory));
+  });
+
+  it('Deve criar um novo Mecanico com sucesso.', async () => {
+    const mechanic = Mechanic.create({
+      name: 'Mecanico Teste',
+      cpf: CPF.create('09253538414'),
+      email: Email.create('email@email.com'),
+      isAvaliable: true,
     });
 
-    it('Deve criar um novo Mecanico com sucesso.', async () => {
-        const mechanic = Mechanic.create({
-            name: 'Mecanico Teste',
-            cpf: CPF.create('09253538414'),
-            email: Email.create('email@email.com'),
-            isAvaliable: true,
-        });
+    mockMechanicRepostory.findByCpf = jest.fn().mockResolvedValue(null);
+    mockMechanicRepostory.findByEmail = jest.fn().mockReturnValue(null);
 
-        mockMechanicRepostory.findByCpf = jest.fn().mockResolvedValue(null);
-        mockMechanicRepostory.findByEmail = jest.fn().mockReturnValue(null);
+    mockMechanicRepostory.create = jest.fn().mockImplementation(async (mechanic) => mechanic);
 
-        mockMechanicRepostory.create = jest.fn().mockImplementation(async (mechanic) => mechanic);
-
-        const result = await createMechanicUSeCase.execute({
-            name: mechanic.name,
-            email: mechanic.email.valueEmail,
-            cpf: mechanic.cpf.valueCpf,
-            isAvaliable: mechanic.isAvaliable ?? true,
-        });
-
-        expect(result.isRight()).toBe(true);
-        expect(result.value).toBeInstanceOf(Mechanic);
-        expect(mockMechanicRepostory.create).toHaveBeenCalled();
+    const result = await createMechanicUSeCase.execute({
+      name: mechanic.name,
+      email: mechanic.email.valueEmail,
+      cpf: mechanic.cpf.valueCpf,
+      isAvaliable: mechanic.isAvaliable ?? true,
     });
 
-    it('Deve dar erro BadRequest ao tentar criar novo mecanico com email duplicado', async () => {
-        const mechanic = Mechanic.create({
-            name: 'Mecanico Teste',
-            cpf: CPF.create('09253538414'),
-            email: Email.create('email@email.com'),
-            isAvaliable: true,
-        });
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toBeInstanceOf(Mechanic);
+    expect(mockMechanicRepostory.create).toHaveBeenCalled();
+  });
 
-        mockMechanicRepostory.findByEmail = jest.fn().mockResolvedValue(mechanic);
-
-        const newMechanic = Mechanic.create({
-            name: 'Teste Mecanico 2',
-            cpf: CPF.create('15524128493'),
-            email: Email.create('email@email.com'),
-            isAvaliable: true,
-        });
-
-        const result = await createMechanicUSeCase.execute({
-            name: newMechanic.name,
-            email: newMechanic.email.valueEmail,
-            cpf: newMechanic.cpf.valueCpf,
-            isAvaliable: newMechanic.isAvaliable,
-        });
-
-        expect(result.isLeft()).toBe(true);
-        expect(result.value).toBeInstanceOf(BadRequest);
-        expect((result.value as BadRequest).statusCode).toEqual(400);
-        expect((result.value as BadRequest).message).toBe('Email ja cadastrado.');
-        expect(mockMechanicRepostory.findByEmail).toHaveBeenCalledWith(mechanic.email.valueEmail);
+  it('Deve dar erro BadRequest ao tentar criar novo mecanico com email duplicado', async () => {
+    const mechanic = Mechanic.create({
+      name: 'Mecanico Teste',
+      cpf: CPF.create('09253538414'),
+      email: Email.create('email@email.com'),
+      isAvaliable: true,
     });
 
-    it('Deve dar erro BadRequest ao tentar criar novo mecanico com cpf duplicado', async () => {
-        const mechanic = Mechanic.create({
-            name: 'Mecanico Teste',
-            cpf: CPF.create('09253538414'),
-            email: Email.create('email@email.com'),
-            isAvaliable: true,
-        });
+    mockMechanicRepostory.findByEmail = jest.fn().mockResolvedValue(mechanic);
 
-        mockMechanicRepostory.findByCpf = jest.fn().mockResolvedValue(mechanic);
-
-        const newMechanic = Mechanic.create({
-            name: 'Teste Mecanico 2',
-            cpf: CPF.create('09253538414'),
-            email: Email.create('email3@email.com'),
-            isAvaliable: true,
-        });
-
-        const result = await createMechanicUSeCase.execute({
-            name: newMechanic.name,
-            email: newMechanic.email.valueEmail,
-            cpf: newMechanic.cpf.valueCpf,
-            isAvaliable: newMechanic.isAvaliable,
-        });
-
-        expect(result.isLeft()).toBe(true);
-        expect(result.value).toBeInstanceOf(BadRequest);
-        expect((result.value as BadRequest).statusCode).toEqual(400);
+    const newMechanic = Mechanic.create({
+      name: 'Teste Mecanico 2',
+      cpf: CPF.create('15524128493'),
+      email: Email.create('email@email.com'),
+      isAvaliable: true,
     });
+
+    const result = await createMechanicUSeCase.execute({
+      name: newMechanic.name,
+      email: newMechanic.email.valueEmail,
+      cpf: newMechanic.cpf.valueCpf,
+      isAvaliable: newMechanic.isAvaliable,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(BadRequest);
+    expect((result.value as BadRequest).statusCode).toEqual(400);
+    expect((result.value as BadRequest).message).toBe('Email ja cadastrado.');
+    expect(mockMechanicRepostory.findByEmail).toHaveBeenCalledWith(mechanic.email.valueEmail);
+  });
+
+  it('Deve dar erro BadRequest ao tentar criar novo mecanico com cpf duplicado', async () => {
+    const mechanic = Mechanic.create({
+      name: 'Mecanico Teste',
+      cpf: CPF.create('09253538414'),
+      email: Email.create('email@email.com'),
+      isAvaliable: true,
+    });
+
+    mockMechanicRepostory.findByCpf = jest.fn().mockResolvedValue(mechanic);
+
+    const newMechanic = Mechanic.create({
+      name: 'Teste Mecanico 2',
+      cpf: CPF.create('09253538414'),
+      email: Email.create('email3@email.com'),
+      isAvaliable: true,
+    });
+
+    const result = await createMechanicUSeCase.execute({
+      name: newMechanic.name,
+      email: newMechanic.email.valueEmail,
+      cpf: newMechanic.cpf.valueCpf,
+      isAvaliable: newMechanic.isAvaliable,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(BadRequest);
+    expect((result.value as BadRequest).statusCode).toEqual(400);
+  });
 });

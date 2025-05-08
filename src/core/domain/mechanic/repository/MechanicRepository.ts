7d19a2bb54be11385228e787dff3cbabd94a2aa6
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-08 00:01:56 -0300

    feat: useCase para listar todos os Mecanicos

--- src/core/domain/mechanic/repository/MechanicRepository.ts
+++ src/core/domain/mechanic/repository/MechanicRepository.ts
@@ -4,4 +4,5 @@ export abstract class MechanicRepository {
   abstract create(entity: Mechanic): Promise<Mechanic>;
   abstract findByEmail(email: string): Promise<Mechanic | null>;
   abstract findByCpf(cpf: string): Promise<Mechanic | null>;
+  abstract findAll(): Promise<Mechanic[]>;
 }

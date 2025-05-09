28d5192fa4a67b463647a5ee91246a741e7bdcdc
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-08 21:08:39 -0300

    test: adicionando camada de teste no useCase deletar mecanico

--- src/tests/unitTests/mechanic/deleteMechanic.spec.ts
+++ src/tests/unitTests/mechanic/deleteMechanic.spec.ts
@@ -3,43 +3,60 @@ import { CPF } from '../../../core/domain/mechanic/objectValue/CPF';
 import { Email } from '../../../core/domain/mechanic/objectValue/Email';
 import { MechanicRepository } from '../../../core/domain/mechanic/repository/MechanicRepository';
 import { DeleteMechanicUseCase } from '../../../core/domain/mechanic/useCase/DeleteMechanic';
+import { BadRequest } from '../../../shared/errors/custom/BadRequest';
 import { NotFound } from '../../../shared/errors/custom/NotFound';
 
 describe('DeleteMechanicUseCase', () => {
-    const mockMechanicRepository = {
-        findById: jest.fn(),
-        delete: jest.fn(),
-    } as unknown as MechanicRepository;
+  const mockMechanicRepository = {
+    findById: jest.fn(),
+    delete: jest.fn(),
+  } as unknown as MechanicRepository;
 
-    let deleteMechanicUseCase: DeleteMechanicUseCase;
+  let deleteMechanicUseCase: DeleteMechanicUseCase;
 
-    beforeEach(() => {
-        jest.clearAllMocks(),
-            (deleteMechanicUseCase = new DeleteMechanicUseCase(mockMechanicRepository));
+  beforeEach(() => {
+    jest.clearAllMocks(),
+      (deleteMechanicUseCase = new DeleteMechanicUseCase(mockMechanicRepository));
+  });
+
+  it('Deve deletar um mecanico', async () => {
+    const mechanic = Mechanic.create({
+      name: 'Mecanico Teste',
+      cpf: CPF.create('09253538414'),
+      email: Email.create('email@email.com'),
+      isAvaliable: true,
     });
 
-    it('Deve deletar um mecanico', async () => {
-        const mechanic = Mechanic.create({
-            name: 'Mecanico Teste',
-            cpf: CPF.create('09253538414'),
-            email: Email.create('email@email.com'),
-            isAvaliable: true,
-        });
+    mockMechanicRepository.findById = jest.fn().mockResolvedValue(mechanic);
+    const result = await deleteMechanicUseCase.execute({ id: mechanic.valueId.valueId });
 
-        mockMechanicRepository.findById = jest.fn().mockResolvedValue(mechanic);
-        const result = await deleteMechanicUseCase.execute({ id: mechanic.valueId.valueId });
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toBe(true);
+  });
 
-        expect(result.isRight()).toBe(true);
-        expect(result.value).toBe(true);
+  it('Deve retornar um erro NotFound ao passar um id invalido', async () => {
+    mockMechanicRepository.findById = jest.fn().mockResolvedValue(null);
+
+    const result = await deleteMechanicUseCase.execute({ id: 'invalid-id' });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NotFound);
+    expect((result.value as NotFound).message).toBe('Mecanico nao encontrado.');
+  });
+
+  it('Deve retornar um erro BadRequest ao tentar deletar um mecanico que com servico ativo', async () => {
+    const mechanic = Mechanic.create({
+      name: 'Mecanico Teste',
+      cpf: CPF.create('09253538414'),
+      email: Email.create('email@email.com'),
+      isAvaliable: false,
     });
 
-    it('Deve retornar um erro NotFound ao passar um id invalido', async () => {
-        mockMechanicRepository.findById = jest.fn().mockResolvedValue(null);
+    mockMechanicRepository.findById = jest.fn().mockResolvedValue(mechanic);
+    const result = await deleteMechanicUseCase.execute({ id: mechanic.valueId.valueId });
 
-        const result = await deleteMechanicUseCase.execute({ id: 'invalid-id' });
-
-        expect(result.isLeft()).toBe(true);
-        expect(result.value).toBeInstanceOf(NotFound);
-        expect((result.value as NotFound).message).toBe('Mecanico nao encontrado.');
-    });
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(BadRequest);
+    expect((result.value as BadRequest).message).toBe('Erro ao deletar mecanico em servico.');
+  });
 });

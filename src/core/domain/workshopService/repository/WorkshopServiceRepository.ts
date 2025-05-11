20b6cfdcb80c90a44ed8adb90e6b9a5ef05f0a42
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-11 13:06:04 -0300

    feat: case de uso para criar um servico da oficina

---
+++ src/core/domain/workshopService/repository/WorkshopServiceRepository.ts
@@ -0,0 +1,6 @@
+import { WorkshopService } from "../entity/WorkshopService";
+
+export abstract class WorkshoServiceRepository {
+    abstract create(entity: WorkshopService): Promise<WorkshopService>;
+    abstract findById(id: string): Promise<WorkshopService | undefined>;
+}

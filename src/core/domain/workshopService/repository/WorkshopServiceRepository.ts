732088feea616d3ba077f864a27b78cb088cecf1
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-20 19:53:03 -0300

    feat: useCase para deletar um servico

--- src/core/domain/workshopService/repository/WorkshopServiceRepository.ts
+++ src/core/domain/workshopService/repository/WorkshopServiceRepository.ts
@@ -4,4 +4,5 @@ export abstract class WorkshoServiceRepository {
   abstract create(entity: WorkshopService): Promise<WorkshopService>;
   abstract findById(id: string): Promise<WorkshopService | undefined>;
   abstract listAll(): Promise<WorkshopService[]>;
+  abstract delete(id: string): Promise<void>;
 }
